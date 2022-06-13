377b0bc53df67323375e87d45eb76f580302f07f
= <hudson@kenzie.com.br>
2022-06-13 13:28:24 -0300

    introducao ao js

---
+++ Demos JS/Aula_Introducao_Js/src/js/script.js
@@ -0,0 +1,64 @@
+
+
+
+//VARIAVEIS
+let     nomePessoa  = "Hudson"
+//const   numero      = 10
+
+//TIPOS DADOS 
+//INT = NUMEROS
+let numero1     = 12
+let numero2     = 10
+let numero3     = 1.23
+
+let resultado = numero1 + numero2
+//console.log(resultado)
+
+//STRINGS => TEXTOS
+let texto        = "Aula de JavaScript"
+let nomeHudson   = "Hudson Carolino"
+let escola       = "Kenzie"
+let numroTexto1  = "10"
+let numroTexto2  = "20"
+
+//BOOLEANOS
+//true  => VERDADEIRO
+//false => FALSO
+
+let texto1 = "texto"
+let numerotexto2 = 12
+let nome3 = "Renato"
+
+//let resultato  = texto1+" "+numerotexto2+"/"+numerotexto2+":"+nome3
+
+//let resultato  = `${texto1} ${numerotexto2} / ${numerotexto2} : ${nome3}`
+
+//COMPARAÇOES 
+//10 > 10 => FALSE 
+//11 < 10 => FALSE
+//11 > 10 => TRUE
+
+//IGUALDADE
+//"Hudson" == "Hudson" => TRUE 
+
+//IGUALDADE + TIPO DE DADO
+//10 === "10" => FALSE
+
+//DIFERENTE
+//"Hudson" !== "hudson" => TRUE
+//"Hudson" !== "Hudson" => FALSE
+
+console.log(10>10)
+
+
+
+
+
+
+
+
+
+
+
+
+
