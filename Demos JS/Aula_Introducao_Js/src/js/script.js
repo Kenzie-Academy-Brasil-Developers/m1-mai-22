5c60e521f5cfd1b4c98390f92f3618df7baf311c
= <hudson@kenzie.com.br>
2022-06-14 13:54:02 -0300

    itrodução if/else

--- Demos JS/Aula_Introducao_Js/src/js/script.js
+++ Demos JS/Aula_Introducao_Js/src/js/script.js
@@ -5,25 +5,37 @@
 let     nomePessoa  = "Hudson"
 //const   numero      = 10
 
+
+
+
+
+
+
+
+
 //TIPOS DADOS 
 //INT = NUMEROS
 let numero1     = 12
 let numero2     = 10
-let numero3     = 1.23
+//let numero3     = 1.23
 
-let resultado = numero1 + numero2
+//let resultado = numero1 + numero2
 //console.log(resultado)
 
+
 //STRINGS => TEXTOS
-let texto        = "Aula de JavaScript"
-let nomeHudson   = "Hudson Carolino"
-let escola       = "Kenzie"
-let numroTexto1  = "10"
-let numroTexto2  = "20"
+let nome      = "Hudson"
+let sobrenome = "Carolino"
+
+//console.log(nome+" "+sobrenome)
+//console.log(`${nome} ${sobrenome}`)
+
 
 //BOOLEANOS
 //true  => VERDADEIRO
 //false => FALSO
+
+
 
 let texto1 = "texto"
 let numerotexto2 = 12
@@ -48,7 +60,35 @@ let nome3 = "Renato"
 //"Hudson" !== "hudson" => TRUE
 //"Hudson" !== "Hudson" => FALSE
 
-console.log(10>10)
+// console.log(10>10)
+
+
+//MAIOR QUE
+//10 > 9 = TRUE
+
+//MENOR QUE 
+//10 < 20  = TRUE
+
+//MAIOR OU IGUAL 
+//10 >= 10 = TRUE 
+
+//MENOR OU IGUAL 
+//11 <= 10 = FALSE
+
+//---- CONDICIONAIS------//
+
+//IDADE
+// let idade = 15
+
+// //SE idade >= 18 => "Atingiu maior idade"
+// if(idade >= 18){
+    
+//     console.log("Atingiu maior idade")
+
+// //SENÃO
+// }else{
+//     console.log("Não atingiu maior idade")
+// }
 
 
 
@@ -56,9 +96,79 @@ console.log(10>10)
 
 
 
+// let usuario = "Abner"
+// let senha   = "12345"
+
+                        
+// if((usuario == "Hudson" || usuario == "Abner") && senha == "12345"){
+
+
+//     console.log("Login com sucesso")
+
+//     if(usuario == "Hudson"){
+
+//         console.log("Admin")
+        
+//     }
+    
+// }else{
+
+//     console.log("Dados incorretos")
+// }
 
 
 
 
+// let num1 = prompt("Digite o primeiro número")
+// let num2 = prompt("Digite o segundo número")
+// let resultado  = 0
+
+// if(num1 > 0 && num2 > 0){
+    
+//     resultado = Number(num1) +  Number(num2)
+//     alert(`O resultado dessa operação é: ${resultado}`)
+
+// }else{
+
+//     alert("Numeros são menores que 0")
+// }
 
 
+// if(10 > 10){
+//     console.log("Passou na condição")
+// }else{
+
+//     console.log("Não passou na condição")
+// }
+
+// +    => SOMA 
+// -    => SUBTRAÇÃO 
+// *    => MULTIPLICAÇÃO 
+// /    => DIVISÃO
+// ++ / +=   => INCREMENTO 
+// -- / -=   => DECREMENTO
+
+// let numero = 3
+// console.log(numero)
+
+// numero -= 1
+// console.log(numero)
+
+// numero -= 1
+// console.log(numero)
+
+// numero -= 1
+// console.log(numero)
+
+
+
+//console.log(10 % 2 == 1)
+
+// if(){
+
+// }else if(){
+
+// }else{
+
+    
+// }
