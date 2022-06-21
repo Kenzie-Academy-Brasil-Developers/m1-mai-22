c4d377ac0edfb1b5a5c8548eb7a8042ee466a8e3
= <hudson@kenzie.com.br>
2022-06-21 14:26:41 -0300

    Introducao arrays loops

---
+++ Demos JS/Aula_Introducao_arrays/src/js/script.js
@@ -0,0 +1,64 @@
+//1 - DECLARAÇÃO DE ARRAY(VAZIO, OU COM INFORMAÇÕES PREÉDEFINIDAS)
+//2 - íNDICES 
+//3 - ACESSANDO INFORMAÇÕES DE UM ARRAY
+//4 - SUBSTIUIR UM VALOR DA POSIÇÃO
+
+
+
+
+
+//             true
+//ENQUANTO(condicional){}
+
+// let cont = 0
+
+// while(cont <= 10){
+
+   
+//     console.log(cont)
+//     cont++
+    
+   
+// }
+
+
+
+ let listaNomes = []
+// //                 0        1         2           3         4     5
+listaNomes.push("Hudson","Gabriel","Anderson", "Daniel", "Thiago")
+
+console.log(listaNomes[0])
+
+
+// //PARA(VARIAVEL CONTROLE; CONDICIONAL; INCREMENTO ){}
+// for(let cont = 0; cont < listaNomes.length; cont = cont + 1){
+
+//     console.log("Nome: "+ listaNomes[cont])
+// }
+
+
+
+// let arrayNumeros = [1,23,435,456,4534,65]
+// function verificaparOuImpar(array){
+//     for(let cont = 0; cont < listaNomes.length; cont = cont + 1){
+
+//         if(){
+
+
+//         }
+//         console.log("Nome: "+ listaNomes[cont])
+//     }
+// }
+// verificaparOuImpar(arrayNumeros)
+
+
+
+
+// function verificaTipo(dado){
+
+//     if(typeof dado == "string"){
+//         console.log("É uma string")
+//     }else if(typeof dado == "number"){
+//         console.log("É uma number")
+//     }
+// }
